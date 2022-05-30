35d694834337cbecedbbc0ec1617606b57d0e61b
Alexey Tsymbal <tsymbal.aleksey@gmail.com>
2022-05-30 14:46:23 +0400

    Add create web3 dialect

--- src/messaging/internal/on-chain-messaging.ts
+++ src/messaging/internal/on-chain-messaging.ts
@@ -1,4 +1,4 @@
-import type { Wallet } from '../../wallet-interfaces';
+import type { Web3Wallet } from '../../wallet-interfaces';
 import type {
   CreateDialectCommand,
   Dialect,
@@ -8,13 +8,68 @@ import type {
   Messaging,
   SendMessageCommand,
 } from './messaging.interface';
+import { DialectMemberScope } from './messaging.interface';
 import type { PublicKey } from '@solana/web3.js';
+import { createDialect } from '@dialectlabs/web3';
+import type { Program } from '@project-serum/anchor';
 
 export class OnChainMessaging implements Messaging {
-  constructor(private readonly wallet: Wallet) {}
+  constructor(private readonly wallet: Web3Wallet, private program: Program) {}
 
-  create(command: CreateDialectCommand): Promise<Dialect> {
-    throw new Error('Not implemented');
+  async create(command: CreateDialectCommand): Promise<Dialect> {
+    const { dialect, publicKey } = await createDialect(
+      this.program,
+      this.wallet,
+      [
+        {
+          publicKey: this.wallet.publicKey,
+          scopes: [
+            command.me.scopes.some((it) => it === DialectMemberScope.ADMIN),
+            command.me.scopes.some((it) => it === DialectMemberScope.WRITE),
+          ],
+        },
+        {
+          publicKey: command.otherMember.publicKey,
+          scopes: [
+            command.otherMember.scopes.some(
+              (it) => it === DialectMemberScope.ADMIN,
+            ),
+            command.otherMember.scopes.some(
+              (it) => it === DialectMemberScope.WRITE,
+            ),
+          ],
+        },
+      ],
+    );
+    const meMember = dialect.members.find((it) =>
+      it.publicKey.equals(this.wallet.publicKey),
+    );
+    const otherMember = dialect.members.find(
+      (it) => !it.publicKey.equals(this.wallet.publicKey),
+    );
+    if (!meMember || !otherMember) {
+      throw new Error('Should not happen');
+    }
+    return new Web3Dialect(
+      this.wallet,
+      this.program,
+      publicKey,
+      {
+        publicKey: meMember.publicKey,
+        scopes: [
+          ...(meMember.scopes[0] ? [DialectMemberScope.ADMIN] : []),
+          ...(meMember.scopes[1] ? [DialectMemberScope.WRITE] : []),
+        ],
+      },
+      {
+        publicKey: otherMember.publicKey,
+        scopes: [
+          ...(otherMember.scopes[0] ? [DialectMemberScope.ADMIN] : []),
+          ...(otherMember.scopes[1] ? [DialectMemberScope.WRITE] : []),
+        ],
+      },
+      dialect.encrypted,
+    );
   }
 
   find(query: FindDialectQuery): Promise<Dialect | null> {
@@ -27,7 +82,9 @@ export class OnChainMessaging implements Messaging {
 }
 
 export class Web3Dialect implements Dialect {
-  private constructor(
+  constructor(
+    readonly wallet: Web3Wallet,
+    readonly program: Program,
     readonly publicKey: PublicKey,
     readonly me: DialectMember,
     readonly otherMember: DialectMember,
