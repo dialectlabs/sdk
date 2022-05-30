c55e5fdf522f50f6a56597708b549c305e2823f5
Alexey Tsymbal <tsymbal.aleksey@gmail.com>
2022-05-30 16:08:33 +0400

    WIP on-chain-messaging, tests pass

--- src/messaging/internal/on-chain-messaging.ts
+++ src/messaging/internal/on-chain-messaging.ts
@@ -1,4 +1,7 @@
-import type { Web3Wallet } from '../../wallet-interfaces';
+import type {
+  Web3EncryptedMessagingWallet,
+  Web3Wallet,
+} from '../../wallet-interfaces';
 import type {
   CreateDialectCommand,
   Dialect,
@@ -10,14 +13,30 @@ import type {
 } from './messaging.interface';
 import { DialectMemberScope } from './messaging.interface';
 import type { PublicKey } from '@solana/web3.js';
-import { createDialect } from '@dialectlabs/web3';
+import {
+  createDialect,
+  deleteDialect,
+  DialectAccount,
+  findDialects,
+  getDialect,
+  sendMessage,
+} from '@dialectlabs/web3';
 import type { Program } from '@project-serum/anchor';
 
 export class OnChainMessaging implements Messaging {
-  constructor(private readonly wallet: Web3Wallet, private program: Program) {}
+  constructor(
+    private readonly wallet: Web3Wallet | Web3EncryptedMessagingWallet,
+    private program: Program,
+  ) {}
 
   async create(command: CreateDialectCommand): Promise<Dialect> {
-    const { dialect, publicKey } = await createDialect(
+    const encrypted = command.encrypted;
+    const encryptionProps = await getEncryptionProps(
+      command.encrypted,
+      this.wallet,
+    );
+    // TODO: same as for web2! accurate handling of dh absence: extract adapter interface + handle sollet
+    const dialectAccount = await createDialect(
       this.program,
       this.wallet,
       [
@@ -40,66 +59,127 @@ export class OnChainMessaging implements Messaging {
           ],
         },
       ],
+      encrypted,
+      encryptionProps,
     );
-    const meMember = dialect.members.find((it) =>
-      it.publicKey.equals(this.wallet.publicKey),
-    );
-    const otherMember = dialect.members.find(
-      (it) => !it.publicKey.equals(this.wallet.publicKey),
-    );
-    if (!meMember || !otherMember) {
-      throw new Error('Should not happen');
-    }
-    return new Web3Dialect(
-      this.wallet,
+    return toWeb3Dialect(dialectAccount, this.wallet, this.program);
+  }
+
+  async find(query: FindDialectQuery): Promise<Dialect | null> {
+    const encryptionProps = await getEncryptionProps(true, this.wallet);
+    const dialectAccount = await getDialect(
       this.program,
-      publicKey,
-      {
-        publicKey: meMember.publicKey,
-        scopes: [
-          ...(meMember.scopes[0] ? [DialectMemberScope.ADMIN] : []),
-          ...(meMember.scopes[1] ? [DialectMemberScope.WRITE] : []),
-        ],
-      },
-      {
-        publicKey: otherMember.publicKey,
-        scopes: [
-          ...(otherMember.scopes[0] ? [DialectMemberScope.ADMIN] : []),
-          ...(otherMember.scopes[1] ? [DialectMemberScope.WRITE] : []),
-        ],
-      },
-      dialect.encrypted,
+      this.wallet.publicKey,
+      encryptionProps,
     );
+    return toWeb3Dialect(dialectAccount, this.wallet, this.program);
   }
 
-  find(query: FindDialectQuery): Promise<Dialect | null> {
-    throw new Error('Not implemented');
-  }
-
-  findAll(): Promise<Dialect[]> {
-    throw new Error('Not implemented');
+  async findAll(): Promise<Dialect[]> {
+    // TODO: rn we have different behavior for web3 and web2 versions: this one always returns empty msgs
+    // TODO: why we don't pass encryptionProps here in protocol?
+    const dialects = await findDialects(this.program, {
+      userPk: this.wallet.publicKey,
+    });
+    return dialects.map((it) => toWeb3Dialect(it, this.wallet, this.program));
   }
 }
 
 export class Web3Dialect implements Dialect {
   constructor(
-    readonly wallet: Web3Wallet,
+    readonly wallet: Web3Wallet | Web3EncryptedMessagingWallet,
     readonly program: Program,
     readonly publicKey: PublicKey,
     readonly me: DialectMember,
     readonly otherMember: DialectMember,
-    readonly encryptionEnabled: boolean,
+    readonly encrypted: boolean,
+    private dialectAccount: DialectAccount,
   ) {}
 
-  delete(): Promise<void> {
-    throw new Error('Not implemented');
+  async delete(): Promise<void> {
+    await deleteDialect(this.program, this.dialectAccount, this.wallet);
   }
 
-  messages(): Promise<Message[]> {
-    throw new Error('Not implemented');
+  async messages(): Promise<Message[]> {
+    const encryptionProps = await getEncryptionProps(true, this.wallet);
+    this.dialectAccount = await getDialect(
+      this.program,
+      this.wallet.publicKey,
+      encryptionProps,
+    );
+    return this.dialectAccount.dialect.messages.map((it) => ({
+      author: it.owner.equals(this.me.publicKey) ? this.me : this.otherMember,
+      timestamp: new Date(it.timestamp),
+      text: it.text,
+    }));
   }
 
-  send(command: SendMessageCommand): Promise<void> {
-    throw new Error('Not implemented');
+  async send(command: SendMessageCommand): Promise<void> {
+    const encryptionProps = await getEncryptionProps(
+      this.encrypted,
+      this.wallet,
+    );
+    await sendMessage(
+      this.program,
+      this.dialectAccount,
+      this.wallet,
+      command.text,
+      encryptionProps,
+    );
   }
+}
+
+function toWeb3Dialect(
+  dialectAccount: DialectAccount,
+  wallet: Web3Wallet | Web3EncryptedMessagingWallet,
+  program: Program,
+) {
+  const { dialect, publicKey } = dialectAccount;
+  const meMember = dialect.members.find((it) =>
+    it.publicKey.equals(wallet.publicKey),
+  );
+  const otherMember = dialect.members.find(
+    (it) => !it.publicKey.equals(wallet.publicKey),
+  );
+  if (!meMember || !otherMember) {
+    throw new Error('Should not happen');
+  }
+  return new Web3Dialect(
+    wallet,
+    program,
+    publicKey,
+    {
+      publicKey: meMember.publicKey,
+      scopes: [
+        ...(meMember.scopes[0] ? [DialectMemberScope.ADMIN] : []),
+        ...(meMember.scopes[1] ? [DialectMemberScope.WRITE] : []),
+      ],
+    },
+    {
+      publicKey: otherMember.publicKey,
+      scopes: [
+        ...(otherMember.scopes[0] ? [DialectMemberScope.ADMIN] : []),
+        ...(otherMember.scopes[1] ? [DialectMemberScope.WRITE] : []),
+      ],
+    },
+    dialect.encrypted,
+    dialectAccount,
+  );
+}
+
+async function getEncryptionProps(
+  encrypted: boolean,
+  wallet: Web3Wallet | Web3EncryptedMessagingWallet,
+) {
+  return (
+    (encrypted &&
+      'diffieHellman' in wallet &&
+      !!wallet.diffieHellman && {
+        diffieHellmanKeyPair: await wallet.diffieHellman(
+          wallet.publicKey.toBytes(),
+        ),
+        ed25519PublicKey: wallet.publicKey.toBytes(),
+      }) ||
+    undefined
+  );
 }
