94fad41757e639ab47a972ab0ac6a2bac5ea60c4
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2022-06-29 01:38:31 +0300

    create deffi hellman stable serialzier&deserializer

--- src/encryption/encryption-keys-store.ts
+++ src/encryption/encryption-keys-store.ts
@@ -38,13 +38,13 @@ class SessionStorageEncryptionKeysStore extends EncryptionKeysStore {
     if (!keys) {
       return null;
     }
-    return JSON.parse(keys) as DiffeHellmanKeys;
+    return deserializeDiffeHellmanKeys(keys);
   }
 
   save(subject: PublicKey, keys: DiffeHellmanKeys): DiffeHellmanKeys {
     sessionStorage.setItem(
       createStorageKey(subject.toBase58()),
-      JSON.stringify(keys),
+      serializeDiffeHellmanKeys(keys),
     );
     return keys;
   }
@@ -56,20 +56,52 @@ class LocalStorageEncryptionKeysStore extends EncryptionKeysStore {
     if (!keys) {
       return null;
     }
-    return JSON.parse(keys) as DiffeHellmanKeys;
+    return deserializeDiffeHellmanKeys(keys) as DiffeHellmanKeys;
   }
 
   save(subject: PublicKey, keys: DiffeHellmanKeys): DiffeHellmanKeys {
     localStorage.setItem(
       createStorageKey(subject.toBase58()),
-      JSON.stringify(keys),
+      serializeDiffeHellmanKeys(keys),
     );
     return keys;
   }
 }
 
-const storageEncryptionKeysPrefix = 'dialect-encryption-keys-';
+// #region json storage
+
+const storageEncryptionKeysPrefix = 'dialect-encryption-keys';
 
 function createStorageKey(subject: string) {
   return `${storageEncryptionKeysPrefix}-${subject}`;
 }
+
+interface DiffeHellmanKeysSerializable {
+  version: 1;
+  keys: {
+    publicKey: number[];
+    secretKey: number[];
+  };
+}
+
+export function serializeDiffeHellmanKeys(keys: DiffeHellmanKeys): string {
+  const toSerialize: DiffeHellmanKeysSerializable = {
+    version: 1,
+    keys: {
+      publicKey: Array.from(keys.publicKey),
+      secretKey: Array.from(keys.secretKey),
+    },
+  };
+
+  return JSON.stringify(toSerialize);
+}
+
+export function deserializeDiffeHellmanKeys(data: string): DiffeHellmanKeys {
+  const deserialized = JSON.parse(data) as DiffeHellmanKeysSerializable;
+  return {
+    publicKey: Uint8Array.from(deserialized.keys.publicKey),
+    secretKey: Uint8Array.from(deserialized.keys.secretKey),
+  };
+}
+
+// #endregion
