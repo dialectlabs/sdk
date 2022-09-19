79d32469d7789d6eba83f010eed3e08afce9a147
Alexey Tsymbal <tsymbal.aleksey@gmail.com>
2022-09-19 12:25:18 +0400

    Fix aptos authN tests

--- packages/sdk/src/aptos/auth/ed25519/aptos-ed25519-authentication-facade-factory.spec.ts
+++ packages/sdk/src/aptos/auth/ed25519/aptos-ed25519-authentication-facade-factory.spec.ts
@@ -4,8 +4,9 @@ import type { AuthenticationFacade } from '../../../core/auth/authentication-fac
 import type { Ed25519TokenSigner } from '../../../core/auth/ed25519/ed25519-token-signer';
 import { NodeDialectWalletAdapter } from '../../wallet-adapter/node-dialect-wallet-adapter';
 import { DialectWalletAdapterEd25519TokenSigner } from './aptos-ed25519-token-signer';
-import type { PublicKey, TokenBody } from '../../../core/auth/auth.interface';
+import type { TokenBody } from '../../../core/auth/auth.interface';
 import { AptosEd25519AuthenticationFacadeFactory } from './aptos-ed25519-authentication-facade-factory';
+import { AptosAccount } from 'aptos';
 
 describe('aptos ed25519 token tests', () => {
   let wallet: DialectWalletAdapterWrapper;
@@ -45,29 +46,53 @@ describe('aptos ed25519 token tests', () => {
     expect(isParsedTokenValid).toBeFalsy();
   });
 
-  // test('when sub compromised returns false', async () => {
-  //   // when
-  //   const token = await authenticationFacade.generateToken(
-  //     Duration.fromObject({ minutes: 5 }),
-  //   );
-  //   const isValid = authenticationFacade.isValid(token);
-  //   expect(isValid).toBeTruthy();
-  //   // then
-  //   const compromisedBody: TokenBody = {
-  //     ...token.body,
-  //     sub: new Keypair().publicKey.toBase58(),
-  //   };
-  //   const compromisedBase64Body = btoa(JSON.stringify(compromisedBody));
-  //   const compromisedToken = authenticationFacade.parseToken(
-  //     token.base64Header +
-  //       '.' +
-  //       compromisedBase64Body +
-  //       '.' +
-  //       token.base64Signature,
-  //   );
-  //   const isParsedTokenValid = authenticationFacade.isValid(compromisedToken);
-  //   expect(isParsedTokenValid).toBeFalsy();
-  // });
+  test('when sub compromised returns false', async () => {
+    // when
+    const token = await authenticationFacade.generateToken(
+      Duration.fromObject({ minutes: 5 }),
+    );
+    const isValid = authenticationFacade.isValid(token);
+    expect(isValid).toBeTruthy();
+    // then
+    const compromisedBody: TokenBody = {
+      ...token.body,
+      sub: new AptosAccount().address().toString(),
+    };
+    const compromisedBase64Body = btoa(JSON.stringify(compromisedBody));
+    const compromisedToken = authenticationFacade.parseToken(
+      token.base64Header +
+        '.' +
+        compromisedBase64Body +
+        '.' +
+        token.base64Signature,
+    );
+    const isParsedTokenValid = authenticationFacade.isValid(compromisedToken);
+    expect(isParsedTokenValid).toBeFalsy();
+  });
+
+  test('when sub jwk compromised returns false', async () => {
+    // when
+    const token = await authenticationFacade.generateToken(
+      Duration.fromObject({ minutes: 5 }),
+    );
+    const isValid = authenticationFacade.isValid(token);
+    expect(isValid).toBeTruthy();
+    // then
+    const compromisedBody: TokenBody = {
+      ...token.body,
+      sub_jwk: new AptosAccount().address().toString(),
+    };
+    const compromisedBase64Body = btoa(JSON.stringify(compromisedBody));
+    const compromisedToken = authenticationFacade.parseToken(
+      token.base64Header +
+        '.' +
+        compromisedBase64Body +
+        '.' +
+        token.base64Signature,
+    );
+    const isParsedTokenValid = authenticationFacade.isValid(compromisedToken);
+    expect(isParsedTokenValid).toBeFalsy();
+  });
 
   test('when exp compromised returns false', async () => {
     // when
@@ -92,73 +117,4 @@ describe('aptos ed25519 token tests', () => {
     const isParsedTokenValid = authenticationFacade.isValid(compromisedToken);
     expect(isParsedTokenValid).toBeFalsy();
   });
-
-  // test('subject and subject public key may not be different', async () => {
-  //   // when
-  //   const subjectPublicKey = new Ed25519PublicKey(
-  //     generateEd25519Keypair().publicKey,
-  //   );
-  //   const signer = new TestDialectWalletAdapterEd25519TokenSigner(
-  //     wallet,
-  //     subjectPublicKey,
-  //     wallet.publicKey,
-  //   );
-  //   authenticationFacade = new aptosEd25519AuthenticationFacadeFactory(
-  //     signer,
-  //   ).get();
-  //   // when
-  //   const token = await authenticationFacade.generateToken(
-  //     Duration.fromObject({ seconds: 100 }),
-  //   );
-  //   // then
-  //   expect(token.body.sub).toBe(subjectPublicKey.toString());
-  //   expect(token.body.sub_jwk).toBe(wallet.publicKey.toString());
-  //   const isValid = authenticationFacade.isValid(token);
-  //   expect(isValid).toBeFalsy();
-  //   const parsedToken = authenticationFacade.parseToken(token.rawValue);
-  //   const isParsedTokenValid = authenticationFacade.isValid(parsedToken);
-  //   expect(isParsedTokenValid).toBeFalsy();
-  // });
-  //
-  // test('subject public key may be absent', async () => {
-  //   // when
-  //   const signer = new TestDialectWalletAdapterEd25519TokenSigner(
-  //     wallet,
-  //     wallet.publicKey,
-  //     undefined!,
-  //   );
-  //   authenticationFacade = new aptosEd25519AuthenticationFacadeFactory(
-  //     signer,
-  //   ).get();
-  //   // when
-  //   const token = await authenticationFacade.generateToken(
-  //     Duration.fromObject({ seconds: 100 }),
-  //   );
-  //   // then
-  //   expect(token.body.sub).toBe(wallet.publicKey.toString());
-  //   expect(token.body.sub_jwk).toBeUndefined();
-  //   const isValid = authenticationFacade.isValid(token);
-  //   expect(isValid).toBeTruthy();
-  //   const parsedToken = authenticationFacade.parseToken(token.rawValue);
-  //   const isParsedTokenValid = authenticationFacade.isValid(parsedToken);
-  //   expect(isParsedTokenValid).toBeTruthy();
-  // });
 });
-
-class TestDialectWalletAdapterEd25519TokenSigner extends DialectWalletAdapterEd25519TokenSigner {
-  constructor(
-    wallet: DialectWalletAdapterWrapper,
-    private readonly _subject: PublicKey,
-    private readonly _subjectPublicKey: PublicKey,
-  ) {
-    super(wallet);
-  }
-
-  override get subject(): PublicKey {
-    return this._subject;
-  }
-
-  override get subjectPublicKey(): PublicKey {
-    return this._subjectPublicKey;
-  }
-}
