f3fd002e0ceab78b554bfe878f1a3ca760d8a37f
Alexey Tsymbal <tsymbal.aleksey@gmail.com>
2022-09-23 14:13:56 +0400

    chore: add notification types configuration example

---
+++ packages/sdk/examples/create-dapp-notificaiton-types.ts
@@ -0,0 +1,32 @@
+import { Dialect, IllegalStateError, NodeDialectWalletAdapter } from '../src';
+
+(async () => {
+  const sdk = Dialect.sdk({
+    // 'production' | 'development' | 'local-development'
+    environment: 'local-development',
+    // DIALECT_SDK_CREDENTIALS env var is consumed by create() and should be used to act on behalf of your dapp private key
+    wallet: NodeDialectWalletAdapter.create(),
+  });
+  const dapp = await sdk.dapps.find();
+  if (!dapp) {
+    throw new IllegalStateError(
+      "Dapp doesn't exist, please create dapp before using it",
+    );
+  }
+  const notificationType = await dapp.notificationTypes.create({
+    humanReadableId: 'announcements',
+    name: 'Announcements',
+    orderingPriority: 0,
+    trigger: 'Notification description or triggering event/conditions',
+    defaultConfig: {
+      enabled: true,
+    },
+  });
+  console.log(JSON.stringify(notificationType));
+  const all = await dapp.notificationTypes.findAll();
+  console.log(JSON.stringify(all));
+  const patched = await dapp.notificationTypes.patch(notificationType.id, {
+    name: 'New feature announcements',
+  });
+  console.log(JSON.stringify(patched));
+})();
