a061a8329f4c3b148dd95ddda0956601ae41cf0a
Alexey Tsymbal <tsymbal.aleksey@gmail.com>
2022-09-24 17:47:27 +0400

    Integrate sdk with data-service for token validation

--- packages/blockchain-sdk-aptos/src/index.ts
+++ packages/blockchain-sdk-aptos/src/index.ts
@@ -2,4 +2,5 @@ export * from './auth/ed25519/aptos-ed25519-authentication-facade-factory';
 export * from './auth/ed25519-payload/aptos-ed25519-payload-authentication-facade-factory';
 export * from './sdk/sdk';
 export * from './wallet-adapter/node-dialect-aptos-wallet-adapter';
+export * from './wallet-adapter/dialect-aptos-wallet-adapter-wrapper';
 export * from './wallet-adapter/dialect-aptos-wallet-adapter.interface';
--- packages/blockchain-sdk-solana/src/index.ts
+++ packages/blockchain-sdk-solana/src/index.ts
@@ -1,4 +1,5 @@
 export * from './wallet-adapter/dialect-solana-wallet-adapter.interface';
+export * from './wallet-adapter/dialect-solana-wallet-adapter-wrapper';
 export * from './wallet-adapter/node-dialect-solana-wallet-adapter';
 export * from './auth/ed25519/solana-ed25519-authentication-facade-factory';
 export * from './auth/tx/solana-tx-authentication-facade-factory';
