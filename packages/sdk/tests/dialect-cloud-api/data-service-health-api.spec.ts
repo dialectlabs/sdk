f42bc7c9cc9fb5a560670a03a4144b2b37cbc886
Alexey Tsymbal <tsymbal.aleksey@gmail.com>
2022-09-24 15:35:49 +0400

    Add test:all script for workspaces

--- packages/sdk/tests/dialect-cloud-api/data-service-health-api.spec.ts
+++ packages/sdk/tests/dialect-cloud-api/data-service-health-api.spec.ts
@@ -1,9 +1,9 @@
 import type { DataServiceHealthApi } from '../../src/dialect-cloud-api/data-service-health-api';
 import { DataServiceApi } from '../../src/dialect-cloud-api/data-service-api';
-import { TokenProvider } from 'auth/token-provider';
-import { Ed25519AuthenticationFacadeFactory } from 'auth/ed25519/ed25519-authentication-facade-factory';
-import { Ed25519TokenSigner } from 'auth/ed25519/ed25519-token-signer';
-import type { AccountAddress } from 'auth/auth.interface';
+import { TokenProvider } from '../../src/auth/token-provider';
+import { Ed25519AuthenticationFacadeFactory } from '../../src/auth/ed25519/ed25519-authentication-facade-factory';
+import { Ed25519TokenSigner } from '../../src/auth/ed25519/ed25519-token-signer';
+import type { AccountAddress } from '../../src/auth/auth.interface';
 
 describe('Data service health api (e2e)', () => {
   const baseUrl = 'http://localhost:8080';
--- packages/sdk/tests/dialect-cloud-api/data-service-wallet-dapp-addresses-api.spec.ts
+++ packages/sdk/tests/dialect-cloud-api/data-service-wallet-dapp-addresses-api.spec.ts
@@ -10,11 +10,11 @@ import {
   DappDto,
   DataServiceDappsApi,
 } from '../../src/dialect-cloud-api/data-service-dapps-api';
-import { TokenProvider } from 'auth/token-provider';
+import { TokenProvider } from '../../src/auth/token-provider';
 import { DataServiceApi } from '../../src/dialect-cloud-api/data-service-api';
-import { Ed25519AuthenticationFacadeFactory } from 'auth/ed25519/ed25519-authentication-facade-factory';
-import { Ed25519TokenSigner } from 'auth/ed25519/ed25519-token-signer';
-import type { AccountAddress } from 'auth/auth.interface';
+import { Ed25519AuthenticationFacadeFactory } from '../../src/auth/ed25519/ed25519-authentication-facade-factory';
+import { Ed25519TokenSigner } from '../../src/auth/ed25519/ed25519-token-signer';
+import type { AccountAddress } from '../../src/auth/auth.interface';
 
 describe('Data service wallet addresses api (e2e)', () => {
   const baseUrl = 'http://localhost:8080';
