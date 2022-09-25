c3ce54bf12ea9408135af1f3c5292a08976b1a96
cbosborn <chris.osborn@gmail.com>
2022-09-25 17:01:26 -0400

    Add more examples for aptos.

---
+++ packages/sdk/examples/get-aptos-messages.ts
@@ -0,0 +1,20 @@
+import { AptosAccount } from 'aptos';
+import type { Thread } from '../src';
+import { createAptosSdk, createAptosThread, getAptosMessages, getAptosThreads, sendMessage } from './helpers';
+
+(async () => {
+  const sdk = createAptosSdk();
+  const recipient = new AptosAccount().pubKey().toString();
+  const thread_ = await createAptosThread(sdk, recipient);
+  await sendMessage(thread_, 'gm');
+  const threads = await getAptosThreads(sdk);
+  // console.log({ threads });
+  if (threads.length < 1 || !threads[0]) {
+    console.log(
+      'You have no threads. Try creating a thread and sending a message first.',
+    );
+    return;
+  }
+  const thread: Thread = threads[0];
+  await getAptosMessages(sdk, thread.id);
+})();
