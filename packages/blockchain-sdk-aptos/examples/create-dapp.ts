25fd3c5bcc25fd539e2a7bc64d91beddc5b96dd5
cbosborn <chris.osborn@gmail.com>
2022-10-24 14:26:22 -0400

    wrap in async

--- packages/blockchain-sdk-aptos/examples/create-dapp.ts
+++ packages/blockchain-sdk-aptos/examples/create-dapp.ts
@@ -16,22 +16,23 @@ import {
 const environment: DialectCloudEnvironment = 'development';
 const aptosAccount = new AptosAccount();
 
-console.log('private key: ' + aptosAccount.toPrivateKeyObject().privateKeyHex);
-console.log('address: ' + aptosAccount.address());
-
-const sdk: DialectSdk<Aptos> = Dialect.sdk(
-  {
-    environment,
-  },
-  AptosSdkFactory.create({
-    wallet: NodeDialectAptosWalletAdapter.create(aptosAccount.signingKey.secretKey),
-  }),
-);
-
-const name = 'Example Aptos Dapp';
-const description = 'My Example Aptos Dapp Description';
-
-const dapp = sdk.dapps.create({
-  name,
-  description,
-});
+(async () => {
+  console.log('private key: ' + aptosAccount.toPrivateKeyObject().privateKeyHex);
+  console.log('address: ' + aptosAccount.address());
+  const sdk: DialectSdk<Aptos> = Dialect.sdk(
+    {
+      environment,
+    },
+    AptosSdkFactory.create({
+      wallet: NodeDialectAptosWalletAdapter.create(aptosAccount.signingKey.secretKey),
+    }),
+  );
+  
+  const name = 'Example Aptos Dapp';
+  const description = 'My Example Aptos Dapp Description';
+  
+  const dapp = await sdk.dapps.create({
+    name,
+    description,
+  });  
+})()
