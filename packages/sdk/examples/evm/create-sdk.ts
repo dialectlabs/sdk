fc6fe3d49f61803441ebce603f449a2808739e38
Aleksei Petrov <alexey.petrov@dialect.to>
2022-12-16 17:14:09 +0400

    feat: add some examples

---
+++ packages/sdk/examples/evm/create-sdk.ts
@@ -0,0 +1,5 @@
+import { createEvmSdk } from './helpers';
+
+// IMPORTANT: must set environment variable DIALECT_SDK_CREDENTIALS
+// to your dapp's EVM messaging wallet keypair
+createEvmSdk();
---
+++ packages/sdk/examples/evm/create-thread.ts
@@ -0,0 +1,9 @@
+import { ethers } from 'ethers';
+import { createEvmSdk, createEvmThread } from './helpers';
+
+(async () => {
+  const sdk = createEvmSdk();
+  const recipient = ethers.Wallet.createRandom();
+  console.log({ recipient: recipient.address });
+  await createEvmThread(sdk, recipient.address);
+})();
