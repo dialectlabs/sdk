91e7a2c5b7968fb4064ccb4db2c4f1ada7dd4f23
Alperen Talaslıoğlu <45829113+iamalperen@users.noreply.github.com>
2023-07-19 18:10:07 +0300

    debt/[CORE-1750]: Versioned Transaction Migration (#107)
    We were using `Transaction` but it's legacy and deprecated. We are not
    migrating our structure to `VersionedTransaction`
    
    ---------
    
    Co-authored-by: Alperen Talaslıoğlu <alperen.talaslioglu@getir.com>

--- packages/blockchain-sdk-solana/src/wallet-adapter/dialect-solana-wallet-adapter-wrapper.ts
+++ packages/blockchain-sdk-solana/src/wallet-adapter/dialect-solana-wallet-adapter-wrapper.ts
@@ -1,4 +1,8 @@
-import type { PublicKey, Transaction } from '@solana/web3.js';
+import type {
+  PublicKey,
+  Transaction,
+  VersionedTransaction,
+} from '@solana/web3.js';
 import type { DialectSolanaWalletAdapter } from './dialect-solana-wallet-adapter.interface';
 import { UnsupportedOperationError } from '@dialectlabs/sdk';
 
@@ -27,7 +31,9 @@ export class DialectSolanaWalletAdapterWrapper
     return new DialectSolanaWalletAdapterWrapper(adapter);
   }
 
-  signTransaction(transaction: Transaction): Promise<Transaction> {
+  signTransaction<T extends Transaction | VersionedTransaction>(
+    transaction: T,
+  ): Promise<T> {
     if (!this.delegate.signTransaction) {
       throw new UnsupportedOperationError(
         'Signing not supported',
@@ -37,14 +43,16 @@ export class DialectSolanaWalletAdapterWrapper
     return this.delegate.signTransaction(transaction);
   }
 
-  signAllTransactions(transaction: Transaction[]): Promise<Transaction[]> {
+  signAllTransactions<T extends Transaction | VersionedTransaction>(
+    transactions: T[],
+  ): Promise<T[]> {
     if (!this.delegate.signAllTransactions) {
       throw new UnsupportedOperationError(
         'Signing not supported',
         'Wallet does not support signing, please use wallet-adapter that supports signAllTransactions() operation.',
       );
     }
-    return this.delegate.signAllTransactions(transaction);
+    return this.delegate.signAllTransactions(transactions);
   }
 
   signMessage(message: Uint8Array): Promise<Uint8Array> {
